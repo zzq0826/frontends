b1c930ae578c40e37041b5686cbd01073b80a692
yqrashawn <namy.19@gmail.com>
2024-12-30 09:34:08 +0800

    feat: use assets/blog for blog.data.json

--- scripts/download-blog-posts.data.json.mjs
+++ scripts/download-blog-posts.data.json.mjs
@@ -1,5 +1,16 @@
-import fs from "fs"
+import fs from "node:fs"
 import fetch from "node-fetch"
+import path from "node:path"
+
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const blogAssetsDir = path.join(__dirname, "..", "src", "assets", "blog")
+
+if (!fs.existsSync(blogAssetsDir)) {
+  fs.mkdirSync(blogAssetsDir, { recursive: true })
+}
 
 const isMainnet = process.env.NEXT_PUBLIC_SCROLL_ENVIRONMENT === "Mainnet"
 
@@ -11,10 +22,10 @@ async function fetchPosts() {
   await Promise.all([
     fetch(buildPostURL("scroll.io"))
       .then(res => res.json())
-      .then(json => fs.writeFileSync("./src/app/blog/[blogId]/data.json", JSON.stringify(json, null, 2))),
+      .then(json => fs.writeFileSync("./src/assets/blog/main.data.json", JSON.stringify(json, null, 2))),
     fetch(buildPostURL("research.scroll.io"))
       .then(res => res.json())
-      .then(json => fs.writeFileSync("./src/research-blog.data.json", JSON.stringify(json, null, 2))),
+      .then(json => fs.writeFileSync("./src/assets/blog/research.data.json", JSON.stringify(json, null, 2))),
   ])
 }
 
